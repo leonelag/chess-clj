20b8c71cdf57bab9f77554243a797421ff31aaa8
Leonel Gayard <leonel.gayard@gmail.com>
2013-08-15 10:23:53 -0300

    Added functions to parse chess board. Added tests for board parsing functions. Added main loop for playing the game over the command line (still incomplete!).

--- chess1/src/chess/core.clj
+++ chess1/src/chess/core.clj
@@ -1,9 +1,105 @@
 (ns chess.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(def pieces
+  {\K :king
+   \Q :queen
+   \N :knight
+   \B :bishop
+   \R :rook
+   \P :pawn})
+
+(defn parse-board
+  "Parse a board from a seq of strings.
+
+   Strings are in the chess notation format. Each character should be one of
+   K,Q,N,B,R,P for King, Queen, kNight, Bishop, Rook, Pawn. Any other letter is
+   considered an empty cell.
+
+   Upper case characters for white pieces, lower case for black pieces.
+
+   The board is a 2D seq where an entry (i,j) contains the piece in cell (i,j)
+   starting furthest from the white player's position."
+  [xs]
+  (let [piece (fn [ch]
+                (if-let [kind (pieces (Character/toUpperCase ch))]
+                  {:kind  kind
+                   :color (if (Character/isUpperCase ch)
+                            :white
+                            :black)
+                   :str   ch        ; string representation of this piece.
+                   }))]
+    (map #(map piece %)
+         xs)))
+
+(defn- print-board
+  "Prints to stdout the board as seen by the white player."
+  [board]
+  (doseq [file board]
+    (println (str/join (map (fn [piece]
+                              (if (nil? piece)
+                                "."
+                                (:str piece)))
+                            file)))))
+
+(def board
+  "The state of a chess board at the beginning of a match."
+  (parse-board ["rnbqkbnr" 
+                "pppppppp"
+                "........"
+                "........"
+                "........"
+                "........"
+                "PPPPPPPP"
+                "RNBQKBNR"]))
+
+(defn- new-game
+  "Creates a new match of chess.
+
+   A game object holds information about the game, such as the current player
+   about to move and the state of the chess board."
+  []
+  {:board board
+   :current-player :white})
+
+(defn- prompt-move
+  "Prints a message to stdout asking for the player's move, reads and returns a
+   line of input from stdin."
+  [player]
+  (println (str player " player, your move ?"))
+  (.readLine *in*))
+
+(defn- parse-move
+  ""
+  [s]
+  )
+
+(defn- move
+  "Updates the game with the move performed by the player."
+;  [game mv]
+;  (let [b (:board game)])
+  []
+  )
+
+(defn eval-move [])
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Starts a game of chess, alternately asking for players' moves on the command
+  line."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+
+  (loop [game (new-game)]
+    (when-not (:over game)
+      (print-board (:board game))
+      (let [str-mv (prompt-move (:current-player game))
+            mv     (eval-move str-mv)]
+
+
+
+        (if (:illegal mv)
+          (do (println (str "Illegal move: " (:cause mv)))
+              (recur game))
+          (recur (move game mv)))))))
--- chess1/test/chess/core_test.clj
+++ chess1/test/chess/core_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [chess.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-parse-board
+  (testing "parse-board should be able to parse a board"
+    (doseq [board [(parse-board ["rnbqkbnr"
+                                 "pppppppp"
+                                 "........"
+                                 "........"
+                                 "........"
+                                 "........"
+                                 "PPPPPPPP"
+                                 "RNBQKBNR"])
+                   chess.core/board]]
+      (is (= 8 (count board))
+          "Board should have 8 ranks")
+      (is (= (repeat 8 8)
+             (map count board))
+          "Each rank should have 8 files")
+      (is (= (repeat 8 :black)
+             (map :color (first board)))
+          "First rank should have 8 black pieces") 
+      (is (= (repeat 8 :white)
+             (map :color (last board)))
+          "Last rank should have 8 white pieces"))))
